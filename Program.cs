32e46e585f1d7c2adaed167d3b3c705a53426c45
netozao <andrefsneto@gmail.com>
2020-01-14 15:46:24 -0700

    Adding comments to the code.

--- Program.cs
+++ Program.cs
@@ -6,30 +6,16 @@ namespace ConsoleApplication
     {
         private static void Main(string[] args)
         {
-            /* Instructions:
-             * Create a new Visual C# Console App using .NET Framework project and name it "ConsoleApplication."
-            Store two variables:
-            Your name / Your location (state or country)
-            Output two WriteLine statements that display those two variables with proper labels (My name is ... , I am from ...) using String Interpolation.
-            Output the current date but not the current time.
-            Output the number of days until Christmas this year and, of course, apply an appropriate label to that output.
-            Add the program example from section 2.1 in the C# Programming Yellow Book by Rob Miles.
-            Add these requirements to the code:
-            Provide appropriate text labels when requesting dimensional input.
-            Cause the program to pause in the console so that the application does not automatically terminate when launched from the Visual Studio run debugger tool.
-            Hint: Consider Console.ReadKey()
-            You do NOT need to add any sort of input validation.
-            Push your completed solution to a GitHub repository.
-            Submit your GitHub repository.
-            */
+            // Create a new Visual C# Console App using .NET Framework project and name it "ConsoleApplication."
+            // Store two variables: Your name / Your location(state or country)
             string myName;
             Console.Write("Please enter your name: ");
             myName = Console.ReadLine();
             string location;
+            // Output two WriteLine statements using String Interpolation.
             Console.Write($"Hi {myName}, where are you from? ");
             location = Console.ReadLine();
             Console.WriteLine($"Great {myName}, I bet {location} is a great place!");
-
             var currentDate = DateTime.Now;
             var futureDate = new DateTime(currentDate.Year, 12, 25);
             if (currentDate.Month == 12 && currentDate.Day >= 26)
@@ -37,8 +23,12 @@ namespace ConsoleApplication
                 futureDate = new DateTime((currentDate.Year + 1), 12, 25);
             }
             var daysToChristmas = (futureDate - currentDate);
+            // Output the current date but not the current time.
+            // Output the number of days until Christmas this year and, of course, apply an appropriate label to that output.
             Console.WriteLine($"The current date is {currentDate.ToString("d")}, and we are only {daysToChristmas.Days + 1} day(s) away from Christmas!");
             Console.WriteLine("Let's calculate the amount of wood and glass required to make a window...");
+            // Add the program example from section 2.1 in the C# Programming Yellow Book by Rob Miles.
+            // Provide appropriate text labels when requesting dimensional input.
             double width, height, woodLength, glassArea;
             string widthString, heightString;
             Console.Write($"Please {myName}, enter the desired width: ");
@@ -53,8 +43,12 @@ namespace ConsoleApplication
             woodLength + " feet");
             Console.WriteLine("The area of the glass is " +
             glassArea + " square metres");
+            // Cause the program to pause in the console so that the application does not automatically terminate when launched from the Visual Studio run debugger tool.
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
+            // You do NOT need to add any sort of input validation.
+            // Push your completed solution to a GitHub repository.
+            // Submit your GitHub repository.
         }
     }
 }
