6cae8724e50c38d4ff93f12cb3c434438be6534e
netozao <andrefsneto@gmail.com>
2020-01-14 13:39:42 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            Console.Write("Please enter your name: ");
+            myName = Console.ReadLine();
+            string location;
+            Console.Write("Where are you from? (City/State) ");
+            location = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + myName);
+            Console.WriteLine($"Your location is {location}");
+
+            var currentDate = DateTime.Now;
+            var futureDate = new DateTime(currentDate.Year, 12, 25);
+            
+            // ------------------------------------           
+
+            Console.WriteLine("The current date is " + currentDate.ToString("d"));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
